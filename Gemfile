54f9ea84b2611df179bd2c7cf4e29d220f970685
Robert Brandin <tumest@gmail.com>
2011-02-20 21:41:10 -0700

    First commit

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.4'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'haml'
+gem 'devise'
+gem 'formtastic'
+gem 'jquery-rails'
+gem 'inherited_resources', '1.1.2'
+gem 'simple_form'
+
+group :test do
+  gem 'webrat'
+  gem 'rspec-rails'
+  gem 'cucumber-rails'
+  gem 'database_cleaner'
+  gem 'capybara'
+  gem 'factory_girl_rails'
+  gem 'shoulda-matchers'
+  gem 'launchy'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chromalog::Application.config.secret_token = 'bc29807dcac6ee4a2a5a7bb61f9d3ad09a475cdbf95d42d17c38f55312f9c1772fc9bb31fe8e540b96851832aadcb741a01242a49053ac5eb2fa62fc74b52771'
