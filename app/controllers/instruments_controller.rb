23dc9a3a054e411e176dd5cfae3bb39e94b36f26
Robert Brandin <tumest@gmail.com>
2011-02-23 15:59:01 -0700

    Added viewport to meta tag to improve appearence on iDevices.

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -4,4 +4,8 @@ class InstrumentsController < InheritedResources::Base
   def create
     create!(:notice => 'New instrument created.') { instruments_path }
   end
+  
+  def update
+    update!(:notice => 'Instrument successfully edited.') { instruments_path }
+  end
 end
