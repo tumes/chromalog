b1bf9148beaac9bfd8a065ce123e939b23b0a758
Robert Brandin <tumest@gmail.com>
2011-02-28 12:48:04 -0700

    Forgot maintenance event validations. Added.

--- app/models/maintenance_event.rb
+++ app/models/maintenance_event.rb
@@ -2,4 +2,6 @@ class MaintenanceEvent < ActiveRecord::Base
   attr_accessible :instrument_id, :date, :problem, :maintenance_performed, :user_id, :result
   belongs_to :instrument
   belongs_to :user
+  
+  validates_presence_of :instrument_id, :date, :problem, :maintenance_event, :user_id, :result
 end
--- spec/models/maintenance_event_spec.rb
+++ spec/models/maintenance_event_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe MaintenanceEvent do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:instrument) }
+  it { should belong_to(:user) }
+  
+  it { should validate_presence_of(:instrument_id) }
+  it { should allow_mass_assignment_of(:instrument_id) }
+  
+  it { should validate_presence_of(:date) }
+  it { should allow_mass_assignment_of(:date) }
+  
+  it { should validate_presence_of(:problem) }
+  it { should allow_mass_assignment_of(:problem) }
+  
+  it { should validate_presence_of(:maintenance_performed) }
+  it { should allow_mass_assignment_of(:maintenance_performed) }
+  
+  it { should validate_presence_of(:user_id) }
+  it { should allow_mass_assignment_of(:user_id) }
+  
+  it { should validate_presence_of(:result) }
+  it { should allow_mass_assignment_of(:result) }
 end
