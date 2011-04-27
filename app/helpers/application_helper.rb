c8effd596ea9d3c866e9b7a115f4e3018792e690
Robert Brandin <tumest@gmail.com>
2011-04-26 22:27:29 -0600

    Major visual and functional overhaul. Not too painful.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def title(page_title)
     content_for(:title) { page_title }
   end
+  
+  def header(header, &block)
+    content_tag(:header, content_tag(:h2, "#{header}")) + 
+    content_tag(:section, :class => "clearfix", &block)
+  end
 end
