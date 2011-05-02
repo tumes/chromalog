4e837d8a6c47851210d4b1b88e42e2848a4931ee
Robert Brandin <tumest@gmail.com>
2011-05-01 20:14:19 -0600

    Testing yet again.

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Chromalog::Application.initialize!
+
+# Login security code, use to keep unwanted users out
+SECURITY_CODE = "testing"
