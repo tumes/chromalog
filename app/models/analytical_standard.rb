9d1c62a01f61d38318c698def0bc42ba94a76cde
Robert Brandin <tumest@gmail.com>
2011-05-01 20:41:50 -0600

    Little fixes.

--- app/models/analytical_standard.rb
+++ app/models/analytical_standard.rb
@@ -1,10 +1,9 @@
 class AnalyticalStandard < ActiveRecord::Base
-  attr_accessible :analytical_method_id, :user_id, :preparation_date, :volume, :notes, :standard_amounts_attributes
+  attr_accessible :analytical_method, :user, :preparation_date, :volume, :notes, :standard_amounts_attributes
   has_many :standard_amounts
-  belongs_to :analytical_method
   
   accepts_nested_attributes_for :standard_amounts
   
-  validates_presence_of :analytical_method_id, :user_id, :preparation_date, :volume
-  validates_numericality_of :analytical_method_id, :user_id, :volume
+  validates_presence_of :analytical_method, :user, :preparation_date, :volume
+  validates_numericality_of :volume
 end
--- app/models/run_log.rb
+++ app/models/run_log.rb
@@ -1,14 +1,12 @@
 class RunLog < ActiveRecord::Base
   belongs_to :instrument
-  belongs_to :analytical_method
-  belongs_to :chromatographic_column
   
   has_many :calibration_levels
   has_many :area_counts, :through => :calibration_levels
   accepts_nested_attributes_for :calibration_levels
   
-  attr_accessible :instrument_id, :analytical_method_id, :chromatographic_column_id, :run_date, :pressure, :flow_rate, :calibration_levels_attributes
-  validates_presence_of :instrument_id, :analytical_method_id, :chromatographic_column_id, :run_date, :pressure, :flow_rate
-  validates_numericality_of :instrument_id, :analytical_method_id, :chromatographic_column_id
+  attr_accessible :instrument_id, :analytical_method, :chromatographic_column, :run_date, :pressure, :flow_rate, :calibration_levels_attributes
+  validates_presence_of :instrument_id, :analytical_method, :chromatographic_column, :run_date, :pressure, :flow_rate
+  validates_numericality_of :instrument_id
   
 end
